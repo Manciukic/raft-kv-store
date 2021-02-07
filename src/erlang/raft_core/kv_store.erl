9ebaad1f1c3e8efd4665bb2811f5cc4c268238ad
Mirko <mirkoletto@gmail.com>
2021-02-07 16:35:11 +0100

    Local implementation of client API

--- src/erlang/raft_core/kv_store.erl
+++ src/erlang/raft_core/kv_store.erl
@@ -9,7 +9,7 @@
 -export([commit_entry/1]).
 
 % Client API
--export([push_entry/1]).
+-export([get/1, get_all/0, set/2, delete/1, delete_all/0]).
 
 
 %%% RAFT API %%%
@@ -18,9 +18,25 @@ commit_entry(Entry) ->
     gen_server:call(?MODULE, {commit_entry, Entry}, infinity).
 
 %%% Client API %%%
-push_entry(Entry) -> 
-    logger:notice("(push_entry) ~p~n", [Entry]),
-    gen_server:call(?MODULE, {push_entry, Entry}, infinity).
+get(Key) ->
+    logger:notice("(get) ~p~n", [Key]),
+    gen_server:call(?MODULE, {get, Key}, infinity).
+
+get_all() ->
+    logger:notice("(get_all) ~n"),
+    gen_server:call(?MODULE, {get_all}, infinity).
+
+set(Key, Value) ->
+    logger:notice("(set) ~p:~p~n", [Key, Value]),
+    gen_server:call(?MODULE, { set, Key, Value }, infinity).
+
+delete(Key) ->
+    logger:notice("(delete) ~p~n", [Key]),
+    gen_server:call(?MODULE, {delete, Key}, infinity).
+
+delete_all() ->
+    logger:notice("(delete_all) ~n", []),
+    gen_server:call(?MODULE, {delete_all}, infinity).
 
 %%% Gen_server behaviour %%%
 start_link() ->
@@ -32,34 +48,59 @@ init(_Args) ->
 
 
 handle_call({commit_entry, Entry}, _From, State) ->
-    NewState = applyEntry(Entry, State),
+    % NewState = set({set, Key, Value}, State),
+    {reply, ok, State};
+
+handle_call({get, Key}, _From, State) ->
+    Result = handle_get(Key, State),
+    {reply, Result, State};
+
+handle_call({get_all}, _From, State) ->
+    Result = handle_get(State),
+    {reply, Result, State};
+
+handle_call({set, Key, Value}, _From, State) ->
+    %raft_statem:add_entry({set, Key, Value}),
+    NewState = handle_set(Key, Value, State),
     {reply, ok, NewState};
 
-handle_call({push_entry, Entry}, _From, State) ->
-    raft_statem:add_entry(Entry),
-    NewState = applyEntry(Entry, State),
+handle_call({delete, Key}, _From, State) ->
+    NewState = handle_delete(Key, State),
+    {reply, ok, NewState};
+
+handle_call({delete_all}, _From, State) ->
+    NewState = handle_delete(State),
     {reply, ok, NewState}.
+    
+handle_cast(Request, State) ->
+    {noreply, State}.
 
-handle_cast({commit_entry, Entry}, State) ->
-    NewState = applyEntry(Entry, State),
-    {noreply, NewState};
+%%% GET %%%
+handle_get(Dict) ->
+    logger:notice("--- Get all ---~n", [ ]),
+    dict:to_list(Dict).
 
-handle_cast({push_entry, Entry}, State) ->
-    raft_statem:add_entry(Entry),
-    NewState = applyEntry(Entry, State),
-    {noreply, NewState}.        
+handle_get(Key, Dict) ->
+    logger:notice("--- Get ~p ---~n", [ Key ]),
+    Result = dict:find(Key, Dict),
+    case Result of
+        {ok, Value} -> Value;
+        error -> error;
+        _ -> error
+    end.
 
 
-%%% Utility functions %%%
-applyEntry({add, Key, Value}, Dict) ->
-    logger:notice("--- Add ~p:~p ---~n", [ Key, Value ]),
-    dict:store(Key, Value, Dict);
-
-applyEntry({delete, Key, Value}, Dict) ->
-    logger:notice("--- Delete ~p:~p ---~n", [ Key, Value ]),
-    dict:erase(Key, Dict);
-
-applyEntry({update, Key, Value}, Dict) ->
-    logger:notice("--- Update ~p:~p ---~n", [ Key, Value ]),
+%%% SET %%%
+handle_set(Key, Value, Dict) ->
+    logger:notice("--- Set ~p:~p ---~n", [ Key, Value ]),
     dict:store(Key, Value, Dict).
 
+
+%%% DELETE %%%
+handle_delete(Key, Dict) ->
+    logger:notice("--- Delete ~p ---~n", [ Key ]),
+    dict:erase(Key, Dict).
+
+handle_delete(Dict) ->
+    logger:notice("--- Delete all ---~n", [ ]),
+    dict:new().
