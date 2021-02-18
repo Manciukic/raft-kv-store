cd502c9399b7f9c16191d402d130d8cae61b4f6d
Mirko <mirkoletto@gmail.com>
2021-02-18 14:03:00 +0100

    Cowboy refactored and tested

--- src/erlang_rest_kv/src/handler.erl
+++ src/erlang_rest_kv/src/handler.erl
@@ -4,7 +4,7 @@
 -module(handler).
 
 -export([init/2]).
--export([allowed_methods/2, content_types_provided/2, handle_req/2, content_types_accepted/2, delete_resource/2]).
+-export([allowed_methods/2, content_types_provided/2, handle_get/2, handle_store/2, content_types_accepted/2, delete_resource/2]).
 
 init(Req, State) ->
 	{cowboy_rest, Req, State}.
@@ -12,71 +12,98 @@ init(Req, State) ->
 allowed_methods(Req, State) ->  
 	{[<<"GET">>, <<"POST">>, <<"PUT">>, <<"DELETE">>], Req, State}.
 
+
+% GET
+
 content_types_provided(Req, State) ->
 	{[
-		{<<"application/json">>, handle_req}
+		{<<"application/json">>, handle_get}
 	], Req, State}.
 
-content_types_accepted(Req, State) ->
-	{[{{<<"application">>, <<"x-www-form-urlencoded">>, '*'}, handle_req}],
-		Req, State}.
 
-handle_req(Req, State) ->
-	logger:debug("handle_req(~p, ~p)~n", [Req, State]),
-	Method = cowboy_req:method(Req),
-	Body = reply(Method, Req),
-	{Body, Req, State}.
-
-delete_resource(Req, State) ->
-	handle_req(Req, State).
-
-
-reply(<<"GET">>, Req) ->
+handle_get(Req, State) ->
 	logger:debug("Received GET Request"),
 	#{key := Key} = cowboy_req:match_qs([{key, [], undefined}], Req),
-	reply_get(Key);
+	{ reply_get(Key), Req, State }.
 
-reply(Method, Req) when ( Method == <<"POST">> ) or ( Method == <<"PUT">> ) ->
+
+reply_get(undefined) ->
+	logger:debug("get_all~n", []),
+	Result = kv_store:get_all(),
+	Data = case Result of
+		error -> [ get_success_body(false) ];
+		_ -> [ get_success_body(true), {<<"data">>, Result } ]
+	end,
+	jsone:encode(Data);
+
+reply_get(Key) ->
+	logger:debug("get(~p)~n", [Key]),
+	Value = kv_store:get(Key),
+	Data = case Value of
+		error -> [ get_success_body(false) ];
+		_ -> [ get_success_body(true), {<<"data">>, [{ Key, Value }]} ]
+	end,
+	logger:debug("get(~p)~n", [Data]),
+	jsone:encode(Data).
+	
+	
+% POST / PUT
+content_types_accepted(Req, State) ->
+	{[{{<<"application">>, <<"x-www-form-urlencoded">>, '*'}, handle_store}],
+		Req, State}.
+
+handle_store(Req, State) ->
 	logger:debug("Received POST/PUT Request"),
 	{ok, PostVals, _} = cowboy_req:read_urlencoded_body(Req),
 	logger:debug("PostVals:~p~n", [PostVals]),
 	Key = proplists:get_value(<<"key">>, PostVals),
 	Value = proplists:get_value(<<"value">>, PostVals),
-	reply_store(Key, Value);
-
-reply(<<"DELETE">>, Req) ->
-	logger:debug("Received DELETE Request"),
-	{ok, PostVals, _} = cowboy_req:read_urlencoded_body(Req),
-	Key = proplists:get_value(<<"key">>, PostVals, null),
-	All = proplists:get_value(<<"all">>, PostVals, false),
-	case All of 
-		false ->
-			reply_delete(Key);
-		_ ->
-			reply_delete_all()
-	end.
-
-reply_get(undefined) ->
-	logger:debug("get~n", []),
-	Dict = kv_store:get_all(),
-	jsone:encode(Dict);
-
-reply_get(Key) ->
-	logger:debug("get(~p)~n", [Key]),
-	Value = kv_store:get(Key),
-	jsone:encode(Value).
+	Success = reply_store(Key, Value),
+	Resp = cowboy_req:set_resp_body(get_encoded_success_body(Success), Req),
+	{get_boolean(Success), Resp, State}.
 
 reply_store(Key, Value) ->
 	logger:debug("set(~p, ~p)~n", [Key, Value]),	
-	kv_store:set(Key, Value),	
-	true.
+	kv_store:set(Key, Value).
+
+% DELETE
+delete_resource(Req, State) ->
+	logger:debug("Received DELETE Request"),
+	{ok, PostVals, _} = cowboy_req:read_urlencoded_body(Req),
+	Key = proplists:get_value(<<"key">>, PostVals, false),
+	All = proplists:get_value(<<"all">>, PostVals, false),
+	Success = case All of 
+		false ->
+			case Key of
+				false -> error;
+				_ -> reply_delete(Key)
+			end;
+		_ ->
+			reply_delete_all()
+	end,
+	Resp = cowboy_req:set_resp_body(get_encoded_success_body(Success), Req),
+	{ok, Resp, State}.
 
 reply_delete(Key) ->
-	logger:debug("delete(~p)~n", [Key]),	
-	kv_store:delete(Key),
-	true.
+	logger:debug("delete(~p)~n", [Key]),
+	% Possible results are ok and error	
+	kv_store:delete(Key).
 
 reply_delete_all() ->
 	logger:debug("delete_all()~n", []),	
-	kv_store:delete_all(),
-	true.
+	% Possible results are ok and error	
+	kv_store:delete_all().
+
+% UTILITY
+get_success_body(Success) when ( Success == ok ) or ( Success == true ) ->
+	{<<"success">>, <<"true">>};
+
+get_success_body(_) ->
+	{<<"success">>, <<"false">>}.
+
+get_encoded_success_body(Success) ->
+	jsone:encode([get_success_body(Success)]).
+
+get_boolean(error) -> false;
+get_boolean(ok) -> true;
+get_boolean(_) -> false.
